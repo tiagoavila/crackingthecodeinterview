139c698b0983634da85c6a5b672dcec451f35118
Tiago <tiagoa@corebrige.net>
2021-11-19 09:20:02 -0300

    rotate matrix finished

--- Chapter1-ArraysAndStrings/RotateMatrix.py
+++ Chapter1-ArraysAndStrings/RotateMatrix.py
@@ -1,11 +1,36 @@
 from numpy import *
 
+
 def rotate_matrix(matrix):
     rows = len(matrix)
     columns = len(matrix[0])
 
-    print(rows)
-    print(columns)
+    if rows == 0 or rows != columns:
+        return
+
+    layers = int(rows / 2)
+
+    for layer in range(layers):
+        first = layer
+        last = rows - 1 - layer
+
+        for index in range(first, last):
+            offset = index - first
+
+            # Save top
+            top = matrix[first][index]
+
+            # left -> top
+            matrix[first][index] = matrix[last - offset][first]
+
+            # bottom -> left
+            matrix[last - offset][first] = matrix[last][last - offset]
+
+            # right -> bottom
+            matrix[last][last - offset] = matrix[index][last]
+
+            # top -> right
+            matrix[index][last] = top
 
     return matrix
 
