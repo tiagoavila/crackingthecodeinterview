8a75c54ebe3b7f2aefc56e0d27085f3faf029ac9
Tiago <tiago.avila09@gmail.com>
2022-02-07 14:20:56 -0300

    removed commented code

--- Chapter4-TreesAndGraphs/validate_binary_search_tree.py
+++ Chapter4-TreesAndGraphs/validate_binary_search_tree.py
@@ -20,22 +20,22 @@ def is_a_binary_search_tree(root_node: TreeNode):
 
 
 def main():
-    # node_1 = TreeNode(1)
-    # node_2 = TreeNode(2)
-    # node_3 = TreeNode(3)
-    # node_4 = TreeNode(4)
-    # node_5 = TreeNode(5)
-    # node_6 = TreeNode(6)
-    #
-    # node_2.left = node_4
-    # node_2.right = node_5
-    #
-    # node_3.left = node_6
-    #
-    # node_1.left = node_2
-    # node_1.right = node_3
-    #
-    # print(f"is this tree a BST? {is_a_binary_search_tree(node_1)}")
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+    node_4 = TreeNode(4)
+    node_5 = TreeNode(5)
+    node_6 = TreeNode(6)
+
+    node_2.left = node_4
+    node_2.right = node_5
+
+    node_3.left = node_6
+
+    node_1.left = node_2
+    node_1.right = node_3
+
+    print(f"is this tree a BST? {is_a_binary_search_tree(node_1)}")
 
     node_1 = TreeNode(1)
     node_2 = TreeNode(2)
