5e2e1c25893d55c90d96d51f6416d7d575073bce
Tiago Avila <tiago.avila09@gmail.com>
2022-02-21 10:06:54 -0300

    bit manipulation exercise

--- Chapter5-BitManipulation/insertion.py
+++ Chapter5-BitManipulation/insertion.py
@@ -1,5 +1,25 @@
+def update_bits(n, m, i, j):
+    all_ones = int('1', 2)
+
+    left = all_ones << (j + 1)
+    right = ((1 << i) - 1)
+
+    mask = left | right
+
+    n_cleared = n & mask
+    m_shifted = m << i
+
+    result = n_cleared | m_shifted
+    print(bin(result))
+
+
 def main():
-    print('test')
+    n = int('10000000000', 2)
+    m = int('10011', 2)
+    i = 2
+    j = 6
+
+    update_bits(n, m, i, j)
 
 
 if __name__ == "__main__":
