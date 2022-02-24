0b7689754161ecf43688a2ef9d6192387a970cf3
Tiago Avila <tiago.avila09@gmail.com>
2022-02-24 10:06:45 -0300

    Black jack card class

---
+++ Chapter7-ObjectOrientedDesign/DeckOfCardsBlackJack/Entities/BlackJackCard.cs
@@ -0,0 +1,35 @@
+﻿namespace DeckOfCardsBlackJack.Entities
+{
+    public class BlackJackCard : Card
+    {
+        public BlackJackCard(int faceValue, SuitEnum suit) : base(faceValue, suit)
+        {
+
+        }
+
+        public override int GetValue()
+        {
+            if (IsAce())
+            {
+                return 1;
+            }
+            
+            if (FaceValue >= 11 || FaceValue <= 13)
+            {
+                return 10;
+            }
+
+            return FaceValue;
+        }
+
+        public bool IsAce()
+        {
+            return FaceValue == 1;
+        }
+
+        public bool IsCardAce()
+        {
+            return FaceValue >= 11 || FaceValue <= 13;
+        }
+    }
+}
