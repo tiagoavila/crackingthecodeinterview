2b6ba87051985fd118a5a0a192b77695069f5494
Tiago Avila <tiago.avila09@gmail.com>
2022-03-01 21:51:03 -0300

    added check for neighbor with opposite color

--- Chapter7-ObjectOrientedDesign/Othello/Entities/Board.cs
+++ Chapter7-ObjectOrientedDesign/Othello/Entities/Board.cs
@@ -54,14 +54,19 @@ namespace Othello.Entities
                 return false;
             }
 
-            // check neighboors, if no neighboor has a color it won't allow to place the piece
-            if (!AreThereAnyNeighboorWithColors(row, column))
+            // check neighbors, if no neighbor has a color it won't allow to place the piece
+            var (thereAreAnyNeighborWithColor, topNeighbor, bottomNeighbor, leftNeighbor, rightNeighbor) = IsThereAnyNeighborWithColors(row, column);
+            if (!thereAreAnyNeighborWithColor)
             {
                 return false;
             }
 
             // also need to check for a valid movement. If I'm player of black pieces,
             // I can only place a piece in a spot where there are white pieces surrounded by black pieces including the spot I want to place the piece
+            if (!IsThereAnyNeighborWithAnOppositeColor(topNeighbor, bottomNeighbor, leftNeighbor, rightNeighbor, color))
+            {
+                return false;
+            }
 
             Pieces[row, column].SetColor(color);
 
@@ -71,45 +76,111 @@ namespace Othello.Entities
         }
 
         /// <summary>
-        /// It checks the top, bottom, left and right neighboors checking if any of them has a color
+        /// It checks the top, bottom, left and right neighbors checking if any of them has a color
         /// </summary>
         /// <param name="row"></param>
         /// <param name="column"></param>
         /// <returns></returns>
-        private bool AreThereAnyNeighboorWithColors(int row, int column)
+        private (
+            bool ThereAreAnyNeighborWithColor,
+            Piece? TopNeighbor,
+            Piece? BottomNeighbor,
+            Piece? LeftNeighbor,
+            Piece? RightNeighbor
+            ) IsThereAnyNeighborWithColors(int row, int column)
         {
-            Piece? topNeighboor = row > 0 ? Pieces[row - 1, column] : null;
-            Piece? bottomNeighboor = row < NUMBER_OF_ROWS ? Pieces[row + 1, column] : null;
-            Piece? leftNeighboor = column > 0 ? Pieces[row, column - 1] : null;
-            Piece? rightNeighboor = column < NUMBER_OF_COLUMNS ? Pieces[row, column + 1] : null;
+            bool thereAreAnyNeighborWithColor = false;
+            var (topNeighbor, bottomNeighbor, leftNeighbor, rightNeighbor) = GetNeighborsOfaSpot(row, column);
 
-            if (topNeighboor != null)
+            if (topNeighbor != null)
             {
-                if (topNeighboor.Color != EnumPieceColor.Empty)
+                if (topNeighbor.Color != EnumPieceColor.Empty)
+                {
+                    thereAreAnyNeighborWithColor = true;
+                }
+            }
+
+            if (bottomNeighbor != null)
+            {
+                if (bottomNeighbor.Color != EnumPieceColor.Empty)
+                {
+                    thereAreAnyNeighborWithColor = true;
+                }
+            }
+
+            if (leftNeighbor != null)
+            {
+                if (leftNeighbor.Color != EnumPieceColor.Empty)
+                {
+                    thereAreAnyNeighborWithColor = true;
+                }
+            }
+
+            if (rightNeighbor != null)
+            {
+                if (rightNeighbor.Color != EnumPieceColor.Empty)
+                {
+                    thereAreAnyNeighborWithColor = true;
+                }
+            }
+
+            return (thereAreAnyNeighborWithColor, topNeighbor, bottomNeighbor, leftNeighbor, rightNeighbor);
+        }
+
+        private (Piece? TopNeighbor, Piece? BottomNeighbor, Piece? LeftNeighbor, Piece? RightNeighbor) GetNeighborsOfaSpot(int row, int column)
+        {
+            Piece? topNeighbor = row > 0 ? Pieces[row - 1, column] : null;
+            Piece? bottomNeighbor = row < NUMBER_OF_ROWS ? Pieces[row + 1, column] : null;
+            Piece? leftNeighbor = column > 0 ? Pieces[row, column - 1] : null;
+            Piece? rightNeighbor = column < NUMBER_OF_COLUMNS ? Pieces[row, column + 1] : null;
+
+            return (topNeighbor, bottomNeighbor, leftNeighbor, rightNeighbor);
+        }
+
+        /// <summary>
+        /// Check for a valid movement. If I'm player of black pieces, 
+        /// I can only place a piece in a spot where there are white pieces surrounded by black pieces including the spot I want to place the piece
+        /// </summary>
+        /// <param name="topNeighbor"></param>
+        /// <param name="bottomNeighbor"></param>
+        /// <param name="leftNeighbor"></param>
+        /// <param name="rightNeighbor"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        private bool IsThereAnyNeighborWithAnOppositeColor(Piece? topNeighbor,
+            Piece? bottomNeighbor,
+            Piece? leftNeighbor,
+            Piece? rightNeighbor, EnumPieceColor color)
+        {
+            EnumPieceColor oppositeColor = color == EnumPieceColor.White ? EnumPieceColor.Black : EnumPieceColor.White;
+
+            if (topNeighbor != null)
+            {
+                if (topNeighbor.Color == oppositeColor)
                 {
                     return true;
                 }
             }
 
-            if (bottomNeighboor != null)
+            if (bottomNeighbor != null)
             {
-                if (bottomNeighboor.Color != EnumPieceColor.Empty)
+                if (bottomNeighbor.Color == oppositeColor)
                 {
                     return true;
                 }
             }
 
-            if (leftNeighboor != null)
+            if (leftNeighbor != null)
             {
-                if (leftNeighboor.Color != EnumPieceColor.Empty)
+                if (leftNeighbor.Color == oppositeColor)
                 {
                     return true;
                 }
             }
 
-            if (rightNeighboor != null)
+            if (rightNeighbor != null)
             {
-                if (rightNeighboor.Color != EnumPieceColor.Empty)
+                if (rightNeighbor.Color == oppositeColor)
                 {
                     return true;
                 }
